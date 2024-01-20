8b3d264525999d3d61903f4a3e0512773aa59098
SN2006 <slavanedelev@gmail.com>
2024-01-20 18:29:04 +0200

    Add miles to kilometers conversion

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,9 +1,23 @@
 package app;
 
+import java.util.Scanner;
+
 public class Main {
+
+    private final static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
         System.out.println("Converter");
+
+        System.out.print("Enter miles: ");
+
+        // Использовал такую конструкцию, чтобы в независимости от ввода пользователя (например: 1,6 или 1.6) программа выполнялась без ошибок
+        double milesToConvert = Double.parseDouble(input.next().replaceAll(",", "."));
+        double kilometers = convertMilesToKilometers(milesToConvert);
+        System.out.format("%.3f miles = %.3f kilometers\n", milesToConvert, kilometers);
     }
 
+    private static double convertMilesToKilometers(double miles){
+        return miles * 1.6;
+    }
 }
